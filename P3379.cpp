58d374f3ed7afafaf00aadf7fcf1d6fa58f78394
learntocode1024 <learntocode1024@outlook.com>
2020-08-20 21:37:18 +0800

    Aug 20 2020

--- P3379.cpp
+++ P3379.cpp
@@ -2,19 +2,12 @@
 // https://www.luogu.com.cn/problem/P3379
 // Created by learntocode1024 on 2020/8/19
 
-struct Node {
-    int key;
-    Node* next = nullptr;
-};
-
-
-Node* Vex[500000];
-Node node_pool[1000000];
-unsigned t = 0;
+const int MAXN = 500000;
+int key[MAXN];
+int nxt[MAXN];
+int ans[MAXN];
+int depth[MAXN];
 
 void add_edge(int fi, int to) {
-    node_pool[t].key = to;
-    node_pool[t].next = Vex[fi];
-    Vex[fi] = node_pool + to;
-
+    
 }
