ac8cc42b138d4961d93c2fcc374b6d8ccd93b155
learntocode1024 <learntocode1024@outlook.com>
2020-08-21 22:37:05 +0800

    AC on Aug 21 2020

--- P3379.cpp
+++ P3379.cpp
@@ -1,17 +1,19 @@
 // luogu/P3379.cpp
 // https://www.luogu.com.cn/problem/P3379
 // Created by learntocode1024 on 2020/8/19
+// AC on Aug 21 2020
+
 #include <cstring>
 #include <cstdio>
 
-const int MAXN = 50;
+const int MAXN = 500005;
 int t = -1, N, M, S;
 int lg[MAXN];
 int fi[MAXN];
-int key[MAXN];
-int nxt[MAXN];
+int key[MAXN*2];
+int nxt[MAXN*2];
 int anc[MAXN][22];
-int depth[MAXN+1];
+int depth[MAXN];
 
 inline void add_edge(int from, int to) {
     ++t;
@@ -20,30 +22,33 @@ inline void add_edge(int from, int to) {
     fi[from] = t;
 }
 
-void dfs(int curr, int fath) {
+void DFS(int curr, int fath) {
+    // can only be called from parents to children
     depth[curr] = depth[fath] + 1;
     anc[curr][0] = fath;
-    for (int i = 1; i <= lg[depth[curr]]; ++i) {
-        anc[curr][i] = anc[anc[curr][i - 1]][i - 1];
-    }
+    // build up!!!
+    for (int j = 1; j <= lg[depth[curr]]; ++j)
+        anc[curr][j] = anc[anc[curr][j - 1]][j - 1];
+    // dfs child
     for (int i = fi[curr]; i != -1; i = nxt[i])
-        if (key[i] != fath) dfs(key[i], curr);
+        if (key[i] != fath) DFS(key[i], curr);
 }
 
 void init() {
+    // calc lg2
+    for (int i = 1; i != N; ++i) {
+        lg[i] = lg[i - 1] + (i >> lg[i - 1] == 1);
+    }
+    // clear fi;
     memset(fi, -1, MAXN*sizeof(int));
-    depth[MAXN] = -1;
-
-    for(int i = 1; i <= N; ++i)
-	  lg[i] = lg[i-1] + (1 << lg[i-1] == i);
-
+    // init tree
     int from, to;
     for (int i = 1; i != N; ++i) {
         scanf("%d %d", &from, &to);
         add_edge(from, to);
         add_edge(to, from);
     }
-    dfs(S, MAXN);
+    DFS(S, S);
 }
 
 int LCA(int a, int b) {
@@ -52,20 +57,22 @@ int LCA(int a, int b) {
         a = b;
         b = c;
     }
-    int delta = depth[a] - depth[b];
-    int cnt = 0;
+    int delta = depth[a] - depth[b], cnt = 0;
     while (delta) {
         if (delta & 1) a = anc[a][cnt];
         ++cnt;
         delta >>= 1;
     }
-    if (a == b) return a;
-    for (int k = lg[depth[a]] - 1; k != 0; --k) {
-        if (anc[a][k] != anc[b][k])
+    // while (depth[a] > depth[b])
+    //     a = anc[a][lg[depth[a] - depth[b]] - 1];
+    if (a == b) return b;
+    for (int k = lg[depth[a]] - 1; k != -1; --k) {
+        if (anc[a][k] != anc[b][k]) {
             a = anc[a][k];
             b = anc[b][k];
+        }
     }
-    return anc[a][0];
+    return anc[b][0];
 }
 
 int main() {
