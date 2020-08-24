2bbbea463d173173161dfc1d48d20971576e1df4
learncoding60 <learncoding60@outlook.com>
2020-08-24 17:15:14 +0800

    Aug 24 2020

---
+++ P1083.cpp
@@ -0,0 +1,11 @@
+// luogu/P1083.cpp
+// https://www.luogu.com.cn/problem/P1083
+// Created by learntocode1024 on Mon Aug 24 2020.
+// 
+const int MX = 1e6 + 5;
+int m, n;
+int r[MX];
+int main() {
+    scanf("%d",)
+    return 0;
+}
