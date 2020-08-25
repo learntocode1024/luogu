a987040a346ce90f3480cce057a603b245e3b85b
learntocode1024 <learntocode1024@outlook.com>
2020-08-25 21:11:23 +0800

    AC on Aug 25 2020

--- P1083.cpp
+++ P1083.cpp
@@ -1,9 +1,11 @@
 // luogu/P1083.cpp
 // https://www.luogu.com.cn/problem/P1083
 // Created by learntocode1024 on Mon Aug 24 2020.
-// AC
+// AC on Aug 25 2020
+
 #include <cstdio>
 #include <cstring>
+
 const int MX = 1e6 +11;
 int m, n;
 int diff[MX];
@@ -12,10 +14,9 @@ int d[MX];
 int s[MX];
 int t[MX];
 
-
 int main() {
-    freopen("P1083_19.in", "r", stdin);
     scanf("%d %d",&n, &m);
+    std::cin >> n >> m;
     for (int i = 0, curr = 0, last = 0; i != n; ++i) {
         scanf("%d", &curr);
         diff[i] = curr - last;
@@ -28,11 +29,13 @@ int main() {
         // t[i] = currt - 1;
         t[i] = currt; // mind this!!
     }
-    int beg = 0,end = m ,mid = (beg + end) / 2;
-    while (mid != end) {
+    int beg = 0,end = m - 1, mid;
+    while (beg != end) {
+        mid = (beg + end) / 2;
         memset(temp, 0, sizeof(temp));
         // judge if mid
         for (int i = 0; i <= mid; ++i) {
+            mid = (beg + end) / 2;
             temp[s[i]] -= d[i];
             temp[t[i]] += d[i];
         }
@@ -46,7 +49,6 @@ int main() {
         }
         if (judge) beg = mid + 1;
         else end = mid;
-        mid = (beg + end) / 2;
     }
     ++beg;
     if (beg == m) printf("0");
