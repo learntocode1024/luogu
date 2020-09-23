5ada119758d61fe0d0414c88a519d12944a7a3e2
learntocode1024 <learntocode1024@outlook.com>
2020-09-23 17:27:29 +0800

    Sep 23 2020

--- P4377.cpp
+++ P4377.cpp
@@ -5,8 +5,12 @@
 //
 
 #include <cstdio>
+#include <algorithm>
 #include <memory.h>
-#define MX_N 255
+using std::max;
+using std::min;
+//#define MX_N 255
+#define MX_N 5
 #define MX_W 1000
 #define MX_RATE 250
 const float EPS = 1e-6;
@@ -14,26 +18,29 @@ const float EPS = 1e-6;
 int n, W;
 int w[MX_N];
 int t[MX_N];
-float dp[MX_W]; // dp to ensure W meets the restriction
+double dp[MX_W]; // dp to ensure W meets the restriction
 
-bool check(float var) { // dp check function
-  memset(dp, 0, sizeof(dp));
-  for (int kN = 0; kN < n; ++kN) {
-
+bool check(double var) { // dp check function
+  for (int i = 0; i < W; ++i) dp[i] = -1e9;
+  for (int kI = 0; kI < n; ++kI) {
+    for (int kW = W; kW >= 0; --kW) {
+      dp[min(W, kW + w[kI])] = max(dp[min(W, kW + w[kI])], dp[kW] +  (float)t[kI] - var * w[kI]);
+    }
   }
+  return dp[W] > 0;
 }
 
 int main() {
   scanf("%d%d", &n, &W);
   for (int kI = 0; kI < n;scanf("%d%d", w + kI, t + kI), ++kI);
   // binary check
-  float L = 0, R = MX_RATE, mid;
+  double L = 0, R = MX_RATE, mid;
   while (R - L > EPS) {
     mid = L + (R - L)/2;
     if (check(mid)) L = mid;
     else R = mid;
   }
-  int ans = (int)(L * 1e3);
+  int ans = (int)(L * 1000);
   printf("%d", ans);
   return 0;
 }
