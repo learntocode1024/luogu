c3a33d4b6f94d790f583043b3a3e8e5f18a7df7d
--get <learntocode1024@outlook.com>
2020-09-25 23:17:29 +0800

    AC on Sep 25 2020

--- P1642.cpp
+++ P1642.cpp
@@ -1,15 +1,16 @@
 // luogu/P1642.cpp
 // https://www.luogu.com.cn/problem/P1642
 // Created by learntocode1024 on Sep 21 2020.
-// 
+// AC on Sep 25 2020
 
 #include <cstdio>
 #include <vector>
 #include <algorithm>
 #include <memory.h>
-#define MX 5
+#define MX 105
 using std::vector;
 using std::max;
+using std::min;
 const float EPS = 1e-3;
 
 int n, m;
@@ -18,14 +19,22 @@ int b[MX];
 float dp[MX][MX];
 vector<int> adj[MX];
 bool vis[MX] = {0};
+int size[MX];
 
 void dfs(int curr) {
   vis[curr] = true;
+  size[curr] = 1;
   for (int child : adj[curr]) {
     if (!vis[child]) {
       dfs(child);
-      for (int kI = n - m; kI > 0; --kI) {
-        dp[curr][kI] = max(dp[curr][kI], dp[child][kI - 1] + dp[curr][1]);
+      size[curr] += size[child];
+      // Below Is WRONG!!! states greater than size[curr] may NOT be updated!!!
+      // for (int k_num = min(n - m, size[curr]); k_num > 0; --k_num) {
+      for (int k_num = min(n - m, size[curr]); k_num > 0; --k_num)
+        for (int select = 1; select < k_num; ++select) {
+          dp[curr][k_num] = max(dp[curr][k_num],
+                                dp[curr][k_num - select] + dp[child][select]);
+        }
       }
     }
   }
@@ -34,8 +43,14 @@ void dfs(int curr) {
 bool check(float mid) {
   memset(vis, 0, sizeof(vis));
   for (int i = 1; i <= n; ++i) dp[i][1] = (float)a[i] - mid*b[i];
+  // DON'T forget the -INF Initialization!!!
+  for (int i = 1; i <= n; ++i)
+    for (int j = 2; j <= n - m; ++ j)
+      dp[i][j] = -1e9;
   dfs(1);
-  return dp[1][n - m] > 0;
+  for (int i = 1; i <= n; ++i)
+    if (dp[i][n - m] > 0) return true;
+  return false;
 }
 
 int main() {
@@ -53,7 +68,7 @@ int main() {
     adj[end].push_back(beg);
   }
   // frac-programming
-  float l = 0, r = 7, mid;
+  float l = 0, r = 1e6, mid;
   while (r - l > EPS) {
     mid = l + (r - l)/2;
     if (check(mid))
